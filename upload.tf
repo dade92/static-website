2baa2027452142970490551b947dbfdd99d9b1c0
dbotti <davide.botti@lastminute.com>
2024-11-29 15:36:11 +0100

    Upload files

---
+++ upload.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_object" "object-upload-html" {
+  for_each     = fileset("uploads/", "*.html")
+  bucket       = aws_s3_bucket.website.bucket
+  key          = each.value
+  source       = "uploads/${each.value}"
+  content_type = "text/html"
+  etag         = filemd5("uploads/${each.value}")
+}
+
+resource "aws_s3_object" "object-upload-js" {
+  for_each     = fileset("uploads/static/js", "*.js")
+  bucket       = aws_s3_bucket.website.bucket
+  key          = "static/js/${each.value}"
+  source       = "uploads/static/${each.value}"
+  content_type = "text/html"
+  etag         = filemd5("uploads/static/${each.value}")
+}
+
+resource "aws_s3_object" "object-upload-css" {
+  for_each     = fileset("uploads/static/css", "*.css")
+  bucket       = aws_s3_bucket.website.bucket
+  key          = "static/css/${each.value}"
+  source       = "uploads/static/${each.value}"
+  content_type = "text/html"
+  etag         = filemd5("uploads/static/${each.value}")
+}
