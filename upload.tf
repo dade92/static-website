91838e8dd449ace009cccf55fce99d5676247d46
dbotti <davide.botti@lastminute.com>
2024-11-29 15:44:43 +0100

    S3 is working now. DNS still with problems

--- upload.tf
+++ upload.tf
@@ -11,16 +11,16 @@ resource "aws_s3_object" "object-upload-js" {
   for_each     = fileset("uploads/static/js", "*.js")
   bucket       = aws_s3_bucket.website.bucket
   key          = "static/js/${each.value}"
-  source       = "uploads/static/${each.value}"
+  source       = "uploads/static/js/${each.value}"
   content_type = "text/html"
-  etag         = filemd5("uploads/static/${each.value}")
+  etag         = filemd5("uploads/static/js/${each.value}")
 }
 
 resource "aws_s3_object" "object-upload-css" {
   for_each     = fileset("uploads/static/css", "*.css")
   bucket       = aws_s3_bucket.website.bucket
   key          = "static/css/${each.value}"
-  source       = "uploads/static/${each.value}"
+  source       = "uploads/static/css/${each.value}"
   content_type = "text/html"
-  etag         = filemd5("uploads/static/${each.value}")
+  etag         = filemd5("uploads/static/css/${each.value}")
 }
